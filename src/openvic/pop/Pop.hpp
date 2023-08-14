f66fbbf1e74fe44370aa8931b5ee007ae0abb2c0
Steve Frampton <40193522+FarmingtonS9@users.noreply.github.com>
2023-08-14 10:32:27 +1000

    Changed pop_size_t to int64 type
    Modified pop_size_t to be an int64, consolidating u_int32_t and int32_t.

--- src/openvic/pop/Pop.hpp
+++ src/openvic/pop/Pop.hpp
@@ -15,15 +15,13 @@ namespace OpenVic {
 	struct Pop {
 		friend struct PopManager;
 
-		using pop_size_t = uint32_t;
-		using pop_signed_size_t = int32_t;
+		using pop_size_t = int64_t;
 
 	private:
 		PopType const& type;
 		Culture const& culture;
 		Religion const& religion;
-		pop_size_t size;
-		pop_signed_size_t num_migrated, pop_daily_change;
+		pop_size_t size, num_migrated, pop_daily_change;
 
 		Pop(PopType const& new_type, Culture const& new_culture, Religion const& new_religion, pop_size_t new_size);
 
@@ -54,13 +52,12 @@ namespace OpenVic {
 			RICH
 		} strata;
 		const sprite_t sprite;
-		const Pop::pop_size_t max_size, merge_max_size, num_promoted, num_demoted;
-		const Pop::pop_signed_size_t num_migrated;
+		const Pop::pop_size_t max_size, merge_max_size, num_promoted, num_demoted, num_migrated;
 		const bool state_capital_only, demote_migrant, is_artisan, is_slave;
 
 		// TODO - rebel composition, life/everyday/luxury needs, country and province migration targets, promote_to targets, ideologies and issues
 
-		PopType(const std::string_view new_identifier, colour_t new_colour, strata_t new_strata, sprite_t new_sprite, Pop::pop_size_t new_max_size, Pop::pop_size_t new_merge_max_size, Pop::pop_size_t new_num_promoted, Pop::pop_size_t new_num_demoted, Pop::pop_signed_size_t new_num_migrated,
+		PopType(const std::string_view new_identifier, colour_t new_colour, strata_t new_strata, sprite_t new_sprite, Pop::pop_size_t new_max_size, Pop::pop_size_t new_merge_max_size, Pop::pop_size_t new_num_promoted, Pop::pop_size_t new_num_demoted, Pop::pop_size_t new_num_migrated,
 			bool new_state_capital_only, bool new_demote_migrant, bool new_is_artisan, bool new_is_slave);
 
 	public:
@@ -72,8 +69,8 @@ namespace OpenVic {
 		Pop::pop_size_t get_merge_max_size() const;
 		Pop::pop_size_t get_num_promoted() const;
 		Pop::pop_size_t get_num_demoted() const;
-		Pop::pop_signed_size_t get_num_migrated() const;
-		Pop::pop_signed_size_t get_pop_daily_change() const;
+		Pop::pop_size_t get_num_migrated() const;
+		Pop::pop_size_t get_pop_daily_change() const;
 		bool get_state_capital_only() const;
 		bool get_demote_migrant() const;
 		bool get_is_artisan() const;
@@ -92,7 +89,7 @@ namespace OpenVic {
 		PopManager();
 
 		return_t add_pop_type(const std::string_view identifier, colour_t new_colour, PopType::strata_t strata, PopType::sprite_t sprite,
-			Pop::pop_size_t max_size, Pop::pop_size_t merge_max_size, Pop::pop_size_t num_promoted, Pop::pop_size_t num_demoted, Pop::pop_signed_size_t num_migrated, bool state_capital_only, bool demote_migrant,
+			Pop::pop_size_t max_size, Pop::pop_size_t merge_max_size, Pop::pop_size_t num_promoted, Pop::pop_size_t num_demoted, Pop::pop_size_t num_migrated, bool state_capital_only, bool demote_migrant,
 			bool is_artisan, bool is_slave);
 		void lock_pop_types();
 		PopType const* get_pop_type_by_identifier(const std::string_view identifier) const;
