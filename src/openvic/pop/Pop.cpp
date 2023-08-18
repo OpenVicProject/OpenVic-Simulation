47af7395f1d2a6a209b214f11d1c45cc1592aefc
Steve Frampton <40193522+FarmingtonS9@users.noreply.github.com>
2023-08-18 13:08:26 +1000

    Move PopType struct attributes to Pop struct attributes
    Moved:
    - num_promoted
    - num_demoted
    - num_migrated
    attributes to Pop struct from PopType struct.
    Added relevant parameters to Pop constructor.
    
    Fixed/moved related functions to Pop struct.

--- src/openvic/pop/Pop.cpp
+++ src/openvic/pop/Pop.cpp
@@ -7,11 +7,14 @@
 
 using namespace OpenVic;
 
-Pop::Pop(PopType const& new_type, Culture const& new_culture, Religion const& new_religion, pop_size_t new_size)
+Pop::Pop(PopType const& new_type, Culture const& new_culture, Religion const& new_religion, pop_size_t new_size, Pop::pop_size_t new_num_promoted, Pop::pop_size_t new_num_demoted, Pop::pop_size_t new_num_migrated)
 	: type { new_type },
 	  culture { new_culture },
 	  religion { new_religion },
-	  size { new_size } {
+	  size { new_size },
+	  num_promoted { new_num_promoted},
+	  num_demoted {new_num_demoted},
+	  num_migrated {new_num_migrated} {
 	assert(size > 0);
 }
 
@@ -31,34 +34,31 @@ Pop::pop_size_t Pop::get_size() const {
 	return size;
 }
 
-Pop::pop_size_t PopType::get_num_promoted() const {
+Pop::pop_size_t Pop::get_num_promoted() const {
 	return num_promoted;
 }
 
-Pop::pop_size_t PopType::get_num_demoted() const {
+Pop::pop_size_t Pop::get_num_demoted() const {
 	return num_demoted;
 }
 
-Pop::pop_size_t PopType::get_num_migrated() const {
+Pop::pop_size_t Pop::get_num_migrated() const {
 	return num_migrated;
 }
 
-Pop::pop_size_t PopType::get_pop_daily_change() const {
-	return PopType::get_num_promoted() - (PopType::get_num_demoted() + PopType::get_num_migrated());
+Pop::pop_size_t Pop::get_pop_daily_change() const {
+	return Pop::get_num_promoted() - (Pop::get_num_demoted() + Pop::get_num_migrated());
 }
 
 PopType::PopType(const std::string_view new_identifier, colour_t new_colour,
 	strata_t new_strata, sprite_t new_sprite,
-	Pop::pop_size_t new_max_size, Pop::pop_size_t new_merge_max_size, Pop::pop_size_t new_num_promoted, Pop::pop_size_t new_num_demoted, Pop::pop_size_t new_num_migrated,
+	Pop::pop_size_t new_max_size, Pop::pop_size_t new_merge_max_size,
 	bool new_state_capital_only, bool new_demote_migrant, bool new_is_artisan, bool new_is_slave)
 	: HasIdentifierAndColour { new_identifier, new_colour, true },
 	  strata { new_strata },
 	  sprite { new_sprite },
 	  max_size { new_max_size },
 	  merge_max_size { new_merge_max_size },
-	  num_promoted { new_num_promoted },
-	  num_demoted { new_num_demoted },
-	  num_migrated { new_num_migrated },
 	  state_capital_only { new_state_capital_only },
 	  demote_migrant { new_demote_migrant },
 	  is_artisan { new_is_artisan },
@@ -108,6 +108,10 @@ static const std::string test_religion = "test_religion";
 static const std::string test_pop_type_poor = "test_pop_type_poor";
 static const std::string test_pop_type_middle = "test_pop_type_middle";
 static const std::string test_pop_type_rich = "test_pop_type_rich";
+static const Pop::pop_size_t test_pop_promoted = 0;
+static const Pop::pop_size_t test_pop_demoted = 0;
+static const Pop::pop_size_t test_pop_migrated = 0;
+
 
 PopManager::PopManager() : pop_types { "pop types" } {
 	culture_manager.add_graphical_culture_type(test_graphical_culture_type);
@@ -126,14 +130,14 @@ PopManager::PopManager() : pop_types { "pop types" } {
 	religion_manager.add_religion(test_religion, 0xFF0000, religion_manager.get_religion_group_by_identifier(test_religion_group), 1, false);
 	religion_manager.lock_religions();
 
-	add_pop_type(test_pop_type_poor, 0xFF0000, PopType::strata_t::POOR, 1, 1, 1, 0, 0, 0, false, false, false, false);
-	add_pop_type(test_pop_type_middle, 0x00FF00, PopType::strata_t::MIDDLE, 1, 1, 1, 0, 0, 0, false, false, false, false);
-	add_pop_type(test_pop_type_rich, 0x0000FF, PopType::strata_t::RICH, 1, 1, 1, 0, 0, 0, false, false, false, false);
+	add_pop_type(test_pop_type_poor, 0xFF0000, PopType::strata_t::POOR, 1, 1, 1, false, false, false, false);
+	add_pop_type(test_pop_type_middle, 0x00FF00, PopType::strata_t::MIDDLE, 1, 1, 1, false, false, false, false);
+	add_pop_type(test_pop_type_rich, 0x0000FF, PopType::strata_t::RICH, 1, 1, 1, false, false, false, false);
 	lock_pop_types();
 }
 
 return_t PopManager::add_pop_type(const std::string_view identifier, colour_t colour, PopType::strata_t strata, PopType::sprite_t sprite,
-	Pop::pop_size_t max_size, Pop::pop_size_t merge_max_size, Pop::pop_size_t num_promoted, Pop::pop_size_t num_demoted, Pop::pop_size_t num_migrated, bool state_capital_only, bool demote_migrant, bool is_artisan, bool is_slave) {
+	Pop::pop_size_t max_size, Pop::pop_size_t merge_max_size, bool state_capital_only, bool demote_migrant, bool is_artisan, bool is_slave) {
 	if (identifier.empty()) {
 		Logger::error("Invalid pop type identifier - empty!");
 		return FAILURE;
@@ -154,7 +158,7 @@ return_t PopManager::add_pop_type(const std::string_view identifier, colour_t co
 		Logger::error("Invalid pop type merge max size for ", identifier, ": ", merge_max_size);
 		return FAILURE;
 	}
-	return pop_types.add_item({ identifier, colour, strata, sprite, max_size, merge_max_size, num_promoted, num_demoted, num_migrated, state_capital_only, demote_migrant, is_artisan, is_slave });
+	return pop_types.add_item({ identifier, colour, strata, sprite, max_size, merge_max_size, state_capital_only, demote_migrant, is_artisan, is_slave });
 }
 
 void PopManager::lock_pop_types() {
@@ -172,9 +176,13 @@ void PopManager::generate_test_pops(Province& province) const {
 		static PopType const& type_rich = *get_pop_type_by_identifier(test_pop_type_rich);
 		static Culture const& culture = *culture_manager.get_culture_by_identifier(test_culture);
 		static Religion const& religion = *religion_manager.get_religion_by_identifier(test_religion);
-		province.add_pop({ type_poor, culture, religion, static_cast<Pop::pop_size_t>(province.get_index() * province.get_index()) * 100 });
-		province.add_pop({ type_middle, culture, religion, static_cast<Pop::pop_size_t>(province.get_index() * province.get_index()) * 50 });
-		province.add_pop({ type_rich, culture, religion, static_cast<Pop::pop_size_t>(province.get_index()) * 1000 });
+		static Pop::pop_size_t const& num_promoted = test_pop_promoted;
+		static Pop::pop_size_t const& num_demoted = test_pop_demoted;
+		static Pop::pop_size_t const& num_migrated = test_pop_migrated;
+
+		province.add_pop({ type_poor, culture, religion, static_cast<Pop::pop_size_t>(province.get_index() * province.get_index()) * 100 ,num_promoted, num_demoted, num_migrated });
+		province.add_pop({ type_middle, culture, religion, static_cast<Pop::pop_size_t>(province.get_index() * province.get_index()) * 50, num_promoted, num_demoted, num_migrated });
+		province.add_pop({ type_rich, culture, religion, static_cast<Pop::pop_size_t>(province.get_index()) * 1000, num_promoted, num_demoted, num_migrated });
 	} else {
 		Logger::error("Cannot generate pops before pop types registry is locked!");
 	}
--- src/openvic/pop/Pop.hpp
+++ src/openvic/pop/Pop.hpp
@@ -21,9 +21,9 @@ namespace OpenVic {
 		PopType const& type;
 		Culture const& culture;
 		Religion const& religion;
-		pop_size_t size, num_migrated, pop_daily_change;
+		pop_size_t size, num_migrated, num_promoted, num_demoted, num_migrated;
 
-		Pop(PopType const& new_type, Culture const& new_culture, Religion const& new_religion, pop_size_t new_size);
+		Pop(PopType const& new_type, Culture const& new_culture, Religion const& new_religion, pop_size_t new_size,pop_size_t new_num_promoted, pop_size_t new_num_demoted, pop_size_t new_num_migrated);
 
 	public:
 		Pop(Pop const&) = delete;
@@ -35,6 +35,10 @@ namespace OpenVic {
 		Culture const& get_culture() const;
 		Religion const& get_religion() const;
 		pop_size_t get_size() const;
+		pop_size_t get_num_promoted() const;
+		pop_size_t get_num_demoted() const;
+		pop_size_t get_num_migrated() const;
+		pop_size_t get_pop_daily_change() const;
 	};
 
 	/* REQUIREMENTS:
@@ -52,12 +56,12 @@ namespace OpenVic {
 			RICH
 		} strata;
 		const sprite_t sprite;
-		const Pop::pop_size_t max_size, merge_max_size, num_promoted, num_demoted, num_migrated;
+		const Pop::pop_size_t max_size, merge_max_size;
 		const bool state_capital_only, demote_migrant, is_artisan, is_slave;
 
 		// TODO - rebel composition, life/everyday/luxury needs, country and province migration targets, promote_to targets, ideologies and issues
 
-		PopType(const std::string_view new_identifier, colour_t new_colour, strata_t new_strata, sprite_t new_sprite, Pop::pop_size_t new_max_size, Pop::pop_size_t new_merge_max_size, Pop::pop_size_t new_num_promoted, Pop::pop_size_t new_num_demoted, Pop::pop_size_t new_num_migrated,
+		PopType(const std::string_view new_identifier, colour_t new_colour, strata_t new_strata, sprite_t new_sprite, Pop::pop_size_t new_max_size, Pop::pop_size_t new_merge_max_size,
 			bool new_state_capital_only, bool new_demote_migrant, bool new_is_artisan, bool new_is_slave);
 
 	public:
@@ -67,10 +71,6 @@ namespace OpenVic {
 		sprite_t get_sprite() const;
 		Pop::pop_size_t get_max_size() const;
 		Pop::pop_size_t get_merge_max_size() const;
-		Pop::pop_size_t get_num_promoted() const;
-		Pop::pop_size_t get_num_demoted() const;
-		Pop::pop_size_t get_num_migrated() const;
-		Pop::pop_size_t get_pop_daily_change() const;
 		bool get_state_capital_only() const;
 		bool get_demote_migrant() const;
 		bool get_is_artisan() const;
@@ -89,7 +89,7 @@ namespace OpenVic {
 		PopManager();
 
 		return_t add_pop_type(const std::string_view identifier, colour_t new_colour, PopType::strata_t strata, PopType::sprite_t sprite,
-			Pop::pop_size_t max_size, Pop::pop_size_t merge_max_size, Pop::pop_size_t num_promoted, Pop::pop_size_t num_demoted, Pop::pop_size_t num_migrated, bool state_capital_only, bool demote_migrant,
+			Pop::pop_size_t max_size, Pop::pop_size_t merge_max_size, bool state_capital_only, bool demote_migrant,
 			bool is_artisan, bool is_slave);
 		void lock_pop_types();
 		PopType const* get_pop_type_by_identifier(const std::string_view identifier) const;
