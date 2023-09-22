d97864faf7288dc1ebf6b0827c353f180b666105
CptAlanSmith <CptAlanSmith@gmail.com>
2023-09-22 23:57:13 +0100

    Move of testing files due to folder reorgs

---
+++ src/openvic-simulation/testing/TestScript.cpp
@@ -0,0 +1,31 @@
+#include <testing/TestScript.hpp>
+
+using namespace OpenVic;
+
+// Getters
+std::vector<Requirement> TestScript::get_requirements() { return requirements; }
+Requirement TestScript::get_requirement_at_index(int index) { return requirements[index]; }
+Requirement TestScript::get_requirement_by_id(std::string id) {
+	for (int i = 0; i < requirements.size(); i++) {
+		if (requirements[i].get_id() == id) return requirements[i];
+	}
+	return Requirement("NULL", "NULL", "NULL");
+}
+std::vector<Requirement> TestScript::get_passed_requirements() {
+	std::vector<Requirement> passed_requirements = std::vector<Requirement>();
+	for (int i = 0; i < requirements.size(); i++) {
+		if (requirements[i].get_pass()) passed_requirements.push_back(requirements[i]);
+	}
+	return passed_requirements;
+}
+std::vector<Requirement> TestScript::get_failed_requirements() {
+	std::vector<Requirement> failed_requirements = std::vector<Requirement>();
+	for (int i = 0; i < requirements.size(); i++) {
+		if (!requirements[i].get_pass()) failed_requirements.push_back(requirements[i]);
+	}
+	return failed_requirements;
+}
+
+// Setters
+void TestScript::set_requirements(std::vector<Requirement> in_requirements) { requirements = in_requirements; }
+void TestScript::add_requirement(Requirement req) {	requirements.push_back(req); }
