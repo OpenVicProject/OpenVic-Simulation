5e28b5e71308e3ff19a3d3a154cf9a975a7b699b
Spartan322 <Megacake1234@gmail.com>
2025-02-11 14:41:35 -0500

    Add country relation functions Add country alliance functions Add countries at war functions Add country military access functions Add country command units functions Add country vision functions Add country opinion functions Add country influence functions Add country discredit functions Add country embassy ban functions
    Add end_alliance action
    Add give_military_access action
    Add end_war_subsidies action
    Add ban_embassy action
    Add remove_from_foreign_sphere action
    Add remove_from_own_sphere action
    Add influence_cost to DiplomaticActionType

--- src/openvic-simulation/diplomacy/CountryRelation.hpp
+++ src/openvic-simulation/diplomacy/CountryRelation.hpp
@@ -1,34 +1,38 @@
 #pragma once
 
+#include <algorithm>
+#include <bit>
 #include <compare>
+#include <cstdint>
+#include <optional>
 #include <tuple>
+#include <type_traits>
 
+#include "openvic-simulation/types/Date.hpp"
 #include "openvic-simulation/types/OrderedContainers.hpp"
 #include "openvic-simulation/utility/Utility.hpp"
 
 namespace OpenVic {
 	struct CountryInstance;
 
-	struct CountryRelationInstanceProxy {
-		std::string_view country_id;
+	using CountryInstancePair = std::pair<CountryInstance const*, CountryInstance const*>;
 
-		CountryRelationInstanceProxy(std::string_view id);
-		CountryRelationInstanceProxy(CountryInstance const* country);
-
-		operator std::string_view() const;
-	};
-
-	struct CountryRelationPair : std::pair<std::string_view, std::string_view> {
-		using base_type = std::pair<std::string_view, std::string_view>;
+	struct UnorderedCountryInstancePair : CountryInstancePair {
+		using base_type = CountryInstancePair;
 		using base_type::base_type;
 
-		inline constexpr auto operator<=>(CountryRelationPair const& rhs) const {
-			using ordering = decltype(OpenVic::utility::three_way(std::tie(first, second), std::tie(rhs.first, rhs.second)));
+		inline constexpr auto operator<=>(UnorderedCountryInstancePair const& rhs) const {
+			const uintptr_t left1 = std::bit_cast<uintptr_t>(first), //
+				left2 = std::bit_cast<uintptr_t>(second), //
+				right1 = std::bit_cast<uintptr_t>(rhs.first), //
+				right2 = std::bit_cast<uintptr_t>(rhs.second);
 
-			auto tied = std::tie(first, second);
-			if (tied == std::tie(rhs.first, rhs.second)) {
+			using ordering = decltype(OpenVic::utility::three_way(std::tie(left1, left2), std::tie(right1, right2)));
+
+			auto tied = std::tie(left1, left2);
+			if (tied == std::tie(right1, right2)) {
 				return ordering::equivalent;
-			} else if (tied == std::tie(rhs.second, rhs.first)) {
+			} else if (tied == std::tie(right2, right1)) {
 				return ordering::equivalent;
 			}
 
@@ -46,24 +50,46 @@ namespace OpenVic {
 			);
 		}
 
-		inline constexpr bool operator==(CountryRelationPair const& rhs) const {
-			auto tied = std::tie(first, second);
-			return tied == std::tie(rhs.first, rhs.second) || tied == std::tie(rhs.second, rhs.first);
+		inline constexpr bool operator==(UnorderedCountryInstancePair const& rhs) const {
+			const uintptr_t left1 = std::bit_cast<uintptr_t>(first), //
+				left2 = std::bit_cast<uintptr_t>(second), //
+				right1 = std::bit_cast<uintptr_t>(rhs.first), //
+				right2 = std::bit_cast<uintptr_t>(rhs.second);
+
+			auto tied = std::tie(left1, left2);
+			return tied == std::tie(right1, right2) || tied == std::tie(right2, right1);
 		}
 	};
 }
 
 namespace std {
 	template<>
-	struct hash<OpenVic::CountryRelationPair> {
-		size_t operator()(OpenVic::CountryRelationPair const& pair) const {
+	struct hash<OpenVic::CountryInstancePair> {
+		size_t operator()(OpenVic::CountryInstancePair const& pair) const {
+			const uintptr_t first = std::bit_cast<uintptr_t>(pair.first), //
+				second = std::bit_cast<uintptr_t>(pair.second);
+
 			std::size_t seed1(0);
-			OpenVic::utility::hash_combine(seed1, pair.first);
-			OpenVic::utility::hash_combine(seed1, pair.second);
+			OpenVic::utility::hash_combine(seed1, first);
+			OpenVic::utility::hash_combine(seed1, second);
+
+			return seed1;
+		}
+	};
+
+	template<>
+	struct hash<OpenVic::UnorderedCountryInstancePair> {
+		size_t operator()(OpenVic::UnorderedCountryInstancePair const& pair) const {
+			const uintptr_t first = std::bit_cast<uintptr_t>(pair.first), //
+				second = std::bit_cast<uintptr_t>(pair.second);
+
+			std::size_t seed1(0);
+			OpenVic::utility::hash_combine(seed1, first);
+			OpenVic::utility::hash_combine(seed1, second);
 
 			std::size_t seed2(0);
-			OpenVic::utility::hash_combine(seed2, pair.second);
-			OpenVic::utility::hash_combine(seed2, pair.first);
+			OpenVic::utility::hash_combine(seed2, second);
+			OpenVic::utility::hash_combine(seed2, first);
 
 			return std::min(seed1, seed2);
 		}
@@ -71,25 +97,147 @@ namespace std {
 }
 
 namespace OpenVic {
-	using country_relation_value_t = int16_t;
-
 	struct CountryRelationManager {
+		using relation_value_type = int16_t;
+		class influence_value_type {
+			static constexpr uint16_t MAX_VALUE = 100; // TODO: implement defines.diplomacy.MAX_INFLUENCE
+			uint16_t value;
+
+		public:
+			constexpr influence_value_type() = default;
+			constexpr influence_value_type(uint16_t value) : value(std::min(value, MAX_VALUE)) {}
+
+#define BIN_OPERATOR(OP) \
+	template<typename T> \
+	friend inline constexpr influence_value_type operator OP(influence_value_type const& lhs, T const& rhs) { \
+		return std::min<uint16_t>(lhs.value OP rhs, MAX_VALUE); \
+	} \
+	template<typename T> \
+	friend inline constexpr influence_value_type operator OP(T const& lhs, influence_value_type const& rhs) { \
+		return std::min<uint16_t>(lhs OP rhs.value, MAX_VALUE); \
+	}
+
+			BIN_OPERATOR(+);
+			BIN_OPERATOR(-);
+			BIN_OPERATOR(*);
+			BIN_OPERATOR(/);
+			BIN_OPERATOR(%);
+
+#undef BIN_OPERATOR
+
+			template<typename T>
+			friend inline constexpr bool operator==(influence_value_type const& lhs, T const& rhs) {
+				return lhs.value == rhs;
+			}
+			template<typename T>
+			friend inline constexpr auto operator<=>(influence_value_type const& lhs, T const& rhs) {
+				return lhs.value <=> rhs;
+			}
+
+
+#define BIN_OPERATOR_EQ(OP) \
+	template<typename T> \
+	friend inline constexpr influence_value_type& operator OP##=(influence_value_type & lhs, T const& rhs) { \
+		lhs.value = std::min<uint16_t>(lhs.value OP rhs, MAX_VALUE); \
+		return lhs; \
+	} \
+	friend inline constexpr influence_value_type& operator OP##=( \
+		influence_value_type & lhs, influence_value_type const& rhs \
+	) { \
+		return lhs OP## = rhs.value; \
+	}
+
+			BIN_OPERATOR_EQ(+);
+			BIN_OPERATOR_EQ(-);
+			BIN_OPERATOR_EQ(*);
+			BIN_OPERATOR_EQ(/);
+			BIN_OPERATOR_EQ(%);
+
+#undef BIN_OPERATOR_EQ
+		};
+
+		enum class OpinionType : int8_t {
+			Hostile = -2, //
+			Opposed,
+			Neutral,
+			Cordial,
+			Friendly,
+			Sphere
+		};
+
 	private:
-		// TODO: reference of manager responsible for storing CountryInstances
-		ordered_map<CountryRelationPair, country_relation_value_t> country_relations;
+#define RELATION_PAIR_MAP(PAIR_TYPE, VALUE_TYPE, NAME, FUNC_NAME, RECIPIENT_CONST) \
+	vector_ordered_map<PAIR_TYPE, VALUE_TYPE> NAME; \
+\
+public: \
+	VALUE_TYPE get_##FUNC_NAME(CountryInstance const* country, CountryInstance const* recipient) const; \
+	VALUE_TYPE& assign_or_get_##FUNC_NAME( \
+		CountryInstance* country, CountryInstance RECIPIENT_CONST* recipient, VALUE_TYPE default_value = {} \
+	); \
+	bool set_##FUNC_NAME(CountryInstance* country, CountryInstance RECIPIENT_CONST* recipient, VALUE_TYPE value); \
+	decltype(NAME)::values_container_type const& get_##FUNC_NAME##_values() const; \
+\
+private:
+
+		RELATION_PAIR_MAP(UnorderedCountryInstancePair, relation_value_type, relations, country_relation, );
+		RELATION_PAIR_MAP(UnorderedCountryInstancePair, bool, alliances, country_alliance, );
+		RELATION_PAIR_MAP(UnorderedCountryInstancePair, bool, at_war, at_war_with, );
+
+		RELATION_PAIR_MAP(CountryInstancePair, bool, military_access, has_military_access_to, const);
+		RELATION_PAIR_MAP(CountryInstancePair, bool, war_subsidies, war_subsidies_to, const);
+		RELATION_PAIR_MAP(CountryInstancePair, bool, command_units, commands_units, const);
+		RELATION_PAIR_MAP(CountryInstancePair, bool, vision, has_vision, const);
+		RELATION_PAIR_MAP(CountryInstancePair, OpinionType, opinions, country_opinion, const);
+		RELATION_PAIR_MAP(CountryInstancePair, influence_value_type, influence, influence_with, const);
+
+		vector_ordered_map<CountryInstancePair, Date> discredits;
+		vector_ordered_map<CountryInstancePair, Date> embassy_bans;
 
 	public:
-		CountryRelationManager(/* TODO: Country Instance Manager Reference */);
+		std::optional<Date> get_discredited_date(CountryInstance const* country, CountryInstance const* recipient) const;
+		Date& assign_or_get_discredited_date(CountryInstance* country, CountryInstance const* recipient, Date default_value);
+		bool set_discredited_date(CountryInstance* country, CountryInstance const* recipient, Date value);
+		decltype(discredits.values_container()) get_discredited_date_values() const;
 
-		bool add_country(CountryRelationInstanceProxy country);
-		bool remove_country(CountryRelationInstanceProxy country);
-
-		country_relation_value_t
-		get_country_relation(CountryRelationInstanceProxy country, CountryRelationInstanceProxy recepient) const;
-		country_relation_value_t*
-		get_country_relation_ptr(CountryRelationInstanceProxy country, CountryRelationInstanceProxy recepient);
-		bool set_country_relation(
-			CountryRelationInstanceProxy country, CountryRelationInstanceProxy recepient, country_relation_value_t value
+		std::optional<Date> get_embassy_banned_date(CountryInstance const* country, CountryInstance const* recipient) const;
+		Date& assign_or_get_embassy_banned_date( //
+			CountryInstance* country, CountryInstance const* recipient, Date default_value
 		);
+		bool set_embassy_banned_date(CountryInstance* country, CountryInstance const* recipient, Date value);
+		decltype(embassy_bans.values_container()) get_embassy_banned_date_values() const;
+
+#undef RELATION_PAIR_MAP
 	};
+}
+
+constexpr OpenVic::CountryRelationManager::OpinionType& operator++(OpenVic::CountryRelationManager::OpinionType& type) {
+	using underlying_type = std::underlying_type_t<OpenVic::CountryRelationManager::OpinionType>;
+	type = static_cast<OpenVic::CountryRelationManager::OpinionType>(static_cast<underlying_type>(type) + 1);
+	type = static_cast<underlying_type>(type) >
+			static_cast<underlying_type>(OpenVic::CountryRelationManager::OpinionType::Sphere) //
+		? OpenVic::CountryRelationManager::OpinionType::Sphere
+		: type;
+	return type;
+}
+
+constexpr OpenVic::CountryRelationManager::OpinionType operator++(OpenVic::CountryRelationManager::OpinionType& type, int) {
+	OpenVic::CountryRelationManager::OpinionType result = type;
+	++type;
+	return result;
+}
+
+constexpr OpenVic::CountryRelationManager::OpinionType& operator--(OpenVic::CountryRelationManager::OpinionType& type) {
+	using underlying_type = std::underlying_type_t<OpenVic::CountryRelationManager::OpinionType>;
+	type = static_cast<OpenVic::CountryRelationManager::OpinionType>(static_cast<underlying_type>(type) - 1);
+	type = static_cast<underlying_type>(type) <
+			static_cast<underlying_type>(OpenVic::CountryRelationManager::OpinionType::Hostile) //
+		? OpenVic::CountryRelationManager::OpinionType::Sphere
+		: type;
+	return type;
+}
+
+constexpr OpenVic::CountryRelationManager::OpinionType operator--(OpenVic::CountryRelationManager::OpinionType& type, int) {
+	OpenVic::CountryRelationManager::OpinionType result = type;
+	--type;
+	return result;
 }
--- src/openvic-simulation/diplomacy/DiplomaticAction.cpp
+++ src/openvic-simulation/diplomacy/DiplomaticAction.cpp
@@ -1,16 +1,20 @@
 #include "DiplomaticAction.hpp"
 
+#include <any>
 #include <string_view>
 
 #include "openvic-simulation/InstanceManager.hpp"
+#include "openvic-simulation/country/CountryInstance.hpp"
+#include "openvic-simulation/diplomacy/CountryRelation.hpp"
+#include "openvic-simulation/types/Date.hpp"
 #include "openvic-simulation/types/IdentifierRegistry.hpp"
 #include "openvic-simulation/utility/Logger.hpp"
 
 using namespace OpenVic;
 
 DiplomaticActionType::DiplomaticActionType(DiplomaticActionType::Initializer&& initializer)
-  : commit_action_caller { std::move(initializer.commit) }, allowed_to_commit { std::move(initializer.allowed) },
-	get_acceptance { std::move(initializer.get_acceptance) } {}
+	: commit_action_caller { std::move(initializer.commit) }, allowed_to_commit { std::move(initializer.allowed) },
+	  get_acceptance { std::move(initializer.get_acceptance) } {}
 
 CancelableDiplomaticActionType::CancelableDiplomaticActionType(CancelableDiplomaticActionType::Initializer&& initializer)
 	: allowed_to_cancel { std::move(initializer.allowed_cancel) }, DiplomaticActionType { std::move(initializer) } {}
@@ -43,8 +47,21 @@ DiplomaticActionTickCache DiplomaticActionManager::create_diplomatic_action_tick
 
 	DiplomaticActionTickCache result { { instance_manager, sender, reciever, context_data }, type };
 	type->visit([&](auto type) {
-		if ((result.allowed_to_commit = type.allowed_to_commit(result.argument))) {
-			result.acceptance = type.get_acceptance(result.argument);
+		CountryRelationManager::influence_value_type* influence = nullptr;
+		if (type.influence_cost != 0) {
+			influence = &result.argument.instance_manager.get_country_relation_manager().assign_or_get_influence_with( //
+				result.argument.sender, result.argument.reciever
+			);
+			if (*influence < type.influence_cost) {
+				return;
+			}
+		}
+		if (!(result.allowed_to_commit = type.allowed_to_commit(result.argument))) {
+			return;
+		}
+		result.acceptance = type.get_acceptance(result.argument);
+		if (influence != nullptr) {
+			*influence -= type.influence_cost;
 		}
 	});
 
@@ -56,10 +73,28 @@ bool DiplomaticActionManager::setup_diplomatic_actions() {
 
 	bool result = true;
 
+	// TODO: implement  _RELATION_ON_ACCEPT & _RELATION_ON_DECLINE
+
 	result &= add_diplomatic_action(
 		"form_alliance",
 		{
-			[](Argument& arg) {},
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().set_country_alliance( //
+						arg.sender, arg.reciever, true
+					);
+				},
+		}
+	);
+	result &= add_diplomatic_action(
+		"end_alliance",
+		{
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().set_country_alliance( //
+						arg.sender, arg.reciever, false
+					);
+				},
 		}
 	);
 	result &= add_diplomatic_action(
@@ -76,29 +111,36 @@ bool DiplomaticActionManager::setup_diplomatic_actions() {
 				},
 		}
 	);
-	result &= add_cancelable_diplomatic_action(
+	result &= add_diplomatic_action(
 		"request_military_access",
 		{
-			.commit = [](Argument& arg) {},
-			.allowed_cancel =
-				[](Argument const& arg) {
-					return true;
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().set_has_military_access_to( //
+						arg.sender, arg.reciever, true
+					);
 				},
 		}
 	);
-	result &= add_diplomatic_action("give_military_access", { [](Argument& arg) {} });
+	result &= add_diplomatic_action( //
+		"give_military_access",
+		{
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().set_has_military_access_to( //
+						arg.sender, arg.reciever, true
+					);
+				},
+		}
+	);
 	result &= add_diplomatic_action(
 		"increase_relations",
 		{
 			.commit =
 				[](Argument& arg) {
-					auto relation = arg.instance_manager.get_country_relation_manager().get_country_relation_ptr(
+					arg.instance_manager.get_country_relation_manager().assign_or_get_country_relation(
 						arg.sender, arg.reciever
-					);
-					if (!relation) {
-						return;
-					}
-					*relation += 25;
+					) += 25; // TODO: implement defines.diplomacy.INCREASERELATION_RELATION_ON_ACCEPT
 				},
 			.allowed =
 				[](Argument const& arg) {
@@ -111,13 +153,9 @@ bool DiplomaticActionManager::setup_diplomatic_actions() {
 		{
 			.commit =
 				[](Argument& arg) {
-					auto relation = arg.instance_manager.get_country_relation_manager().get_country_relation_ptr(
+					arg.instance_manager.get_country_relation_manager().assign_or_get_country_relation(
 						arg.sender, arg.reciever
-					);
-					if (!relation) {
-						return;
-					}
-					*relation -= 25;
+					) -= 25; // TODO: implement defines.diplomacy.DECREASERELATION_RELATION_ON_ACCEPT
 				},
 			.allowed =
 				[](Argument const& arg) {
@@ -128,73 +166,197 @@ bool DiplomaticActionManager::setup_diplomatic_actions() {
 	result &= add_diplomatic_action(
 		"war_subsidies",
 		{
-			[](Argument& arg) {},
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().set_war_subsidies_to( //
+						arg.sender, arg.reciever, true
+					);
+				},
+		}
+	);
+	result &= add_diplomatic_action(
+		"end_war_subsidies",
+		{
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().set_war_subsidies_to( //
+						arg.sender, arg.reciever, false
+					);
+				},
 		}
 	);
 	result &= add_diplomatic_action(
 		"declare_war",
 		{
-			[](Argument& arg) {},
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().set_at_war_with( //
+						arg.sender, arg.reciever, true
+					);
+				},
 		}
 	);
 	result &= add_diplomatic_action(
 		"offer_peace",
 		{
-			[](Argument& arg) {},
+			.commit = [](Argument& arg) {},
 		}
 	);
 	result &= add_diplomatic_action(
 		"command_units",
 		{
-			[](Argument& arg) {},
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().assign_or_get_commands_units( //
+						arg.sender, arg.reciever
+					) = true;
+				},
 		}
 	);
 	result &= add_diplomatic_action(
 		"discredit",
 		{
-			[](Argument& arg) {},
+			.influence_cost = 25,
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().set_discredited_date( //
+						arg.sender, arg.reciever, arg.instance_manager.get_today() + 180
+					); // TODO: implement defines.diplomacy.DISCREDIT_DAYS
+				},
 		}
 	);
 	result &= add_diplomatic_action(
 		"expel_advisors",
 		{
-			[](Argument& arg) {},
+			.influence_cost = 50, // TODO: implement defines.diplomacy.EXPELADVISORS_INFLUENCE_COST
+			.commit = [](Argument& arg) {},
+		}
+	);
+	result &= add_diplomatic_action(
+		"ban_embassy",
+		{
+			.influence_cost = 65,
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().set_embassy_banned_date( //
+						arg.sender, arg.reciever, arg.instance_manager.get_today() + Timespan::from_years(1)
+					); // TODO: implement defines.diplomacy.BANEMBASSY_DAYS
+				},
 		}
 	);
 	result &= add_diplomatic_action(
 		"increase_opinion",
 		{
-			[](Argument& arg) {},
+			.influence_cost = 50,
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().assign_or_get_country_opinion( //
+						arg.sender, arg.reciever
+					)++;
+				},
+			.allowed =
+				[](Argument const& arg) {
+					return true;
+				},
 		}
 	);
 	result &= add_diplomatic_action(
 		"decrease_opinion",
 		{
-			[](Argument& arg) {},
+			.influence_cost = 50,
+			.commit =
+				[](Argument& arg) {
+					--arg.instance_manager.get_country_relation_manager().assign_or_get_country_opinion( //
+						std::any_cast<CountryInstance*>(arg.context_data), arg.reciever
+					);
+				},
+			.allowed =
+				[](Argument const& arg) {
+					return true;
+				},
 		}
 	);
 	result &= add_diplomatic_action(
 		"add_to_sphere",
 		{
-			[](Argument& arg) {},
+			.influence_cost = 100,
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().assign_or_get_country_opinion( //
+						arg.sender, arg.reciever
+					) = CountryRelationManager::OpinionType::Sphere;
+				},
+			.allowed =
+				[](Argument const& arg) {
+					return true;
+				},
 		}
 	);
 	result &= add_diplomatic_action(
-		"remove_from_sphere",
+		"remove_from_foreign_sphere",
 		{
-			[](Argument& arg) {},
+			.influence_cost = 100,
+			.commit =
+				[](Argument& arg) {
+					--arg.instance_manager.get_country_relation_manager().assign_or_get_country_opinion( //
+						std::any_cast<CountryInstance*>(arg.context_data), arg.reciever
+					);
+					arg.instance_manager.get_country_relation_manager().assign_or_get_country_relation( //
+						arg.sender, arg.reciever
+					) -= 10;
+				},
+			.allowed =
+				[](Argument const& arg) {
+					return true;
+				},
+		}
+	);
+	result &= add_diplomatic_action(
+		"remove_from_own_sphere",
+		{
+			.influence_cost = 100,
+			.commit =
+				[](Argument& arg) {
+					--arg.instance_manager.get_country_relation_manager().assign_or_get_country_opinion( //
+						arg.sender, arg.reciever
+					);
+					arg.instance_manager.get_country_relation_manager().assign_or_get_country_relation( //
+						arg.sender, arg.reciever
+					) -= 10;
+					// TODO: implement REMOVEFROMSPHERE_PRESTIGE_COST and REMOVEFROMSPHERE_INFAMY_COST
+				},
+			.allowed =
+				[](Argument const& arg) {
+					return true;
+				},
 		}
 	);
 	result &= add_diplomatic_action(
 		"justify_war",
 		{
-			[](Argument& arg) {},
+			.commit = [](Argument& arg) {},
 		}
 	);
 	result &= add_diplomatic_action(
 		"give_vision",
 		{
-			[](Argument& arg) {},
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().set_has_vision( //
+						arg.sender, arg.reciever, true
+					);
+				},
+		}
+	);
+	result &= add_diplomatic_action(
+		"remove_vision",
+		{
+			.commit =
+				[](Argument& arg) {
+					arg.instance_manager.get_country_relation_manager().set_has_vision( //
+						arg.sender, arg.reciever, false
+					);
+				},
 		}
 	);
 	diplomatic_action_types.lock();
