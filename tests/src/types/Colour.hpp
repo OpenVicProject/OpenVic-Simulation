2ef0d2ac355ea5eda72fce18ddc657c33b120e81
Spartan322 <Megacake1234@gmail.com>
2025-03-10 10:02:07 -0400

    Extract `snitch::append` colour print to tests/src/types/Colour.hpp

---
+++ tests/src/types/Colour.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cmath>
+
+#include "openvic-simulation/types/Colour.hpp"
+
+#include <snitch/snitch_string.hpp>
+#include <snitch/snitch_string_utility.hpp>
+
+namespace snitch {
+	template<typename ValueT, typename ColourIntT, typename ColourTraits>
+	inline static bool append( //
+		snitch::small_string_span ss, OpenVic::basic_colour_t<ValueT, ColourIntT, ColourTraits> const& s
+	) {
+		if constexpr (std::decay_t<decltype(s)>::colour_traits::has_alpha) {
+			return append(ss, "(", (int64_t)s.red, ",", (int64_t)s.green, ",", (int64_t)s.blue, ",", (int64_t)s.alpha, ")");
+		} else {
+			return append(ss, "(", (int64_t)s.red, ",", (uint64_t)s.green, ",", (int64_t)s.blue, ")");
+		}
+	}
+}
