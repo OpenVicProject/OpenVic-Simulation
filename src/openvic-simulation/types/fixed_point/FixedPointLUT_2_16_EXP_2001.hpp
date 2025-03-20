77d94fc5034e38f2470ed018cdf276ae559b80ac
Spartan322 <Megacake1234@gmail.com>
2025-03-20 11:36:09 -0400

    Move LUT fixed point generator scripts to misc/scripts Overhaul LUT fixed point headers

--- src/openvic-simulation/types/fixed_point/FixedPointLUT_2_16_EXP_2001.hpp
+++ src/openvic-simulation/types/fixed_point/FixedPointLUT_2_16_EXP_2001.hpp
@@ -1,25 +1,36 @@
+// This file was generated using the `misc/scripts/exp_lut_generator.py` script with `-e 2001`.
 
-static constexpr int64_t LUT_2_16_EXP_2001_DIVISOR = 65536;
-static constexpr size_t LUT_2_16_EXP_2001_SIZE = 19;
+#pragma once
 
-static constexpr std::array<int64_t, LUT_2_16_EXP_2001_SIZE> LUT_2_16_EXP_2001 {
-	65544,
-	65551,
-	65566,
-	65597,
-	65658,
-	65780,
-	66024,
-	66516,
-	67511,
-	69546,
-	73801,
-	83108,
-	105392,
-	169487,
-	438320,
-	2931592,
-	131137536,
-	262406209536,
-	1050674725388353536
-};
+#include <array>
+#include <cstddef>
+#include <cstdint>
+
+// clang-format off
+namespace OpenVic::_detail::LUT {
+	static constexpr uint64_t _2_16_EXP_2001_DIVISOR = 65536;
+	static constexpr size_t _2_16_EXP_2001_SIZE = 19;
+
+	static constexpr std::array<int64_t, _2_16_EXP_2001_SIZE> _2_16_EXP_2001 {
+		65544,
+		65551,
+		65566,
+		65597,
+		65658,
+		65780,
+		66024,
+		66516,
+		67511,
+		69546,
+		73801,
+		83108,
+		105392,
+		169487,
+		438320,
+		2931592,
+		131137536,
+		262406209536,
+		1050674725388353536
+	};
+}
+// clang-format on
