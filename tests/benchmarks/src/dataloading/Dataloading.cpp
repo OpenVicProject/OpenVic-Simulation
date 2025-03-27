967d1f1e674df89911cd32904f8343f0e54b2572
Spartan322 <Megacake1234@gmail.com>
2025-03-27 17:36:21 -0400

    Add Dataloading benchmarks Add nanobench as benchmarks dependency 	See https://github.com/Spartan322/nanobench/tree/disable/exceptions-and-rtti

---
+++ tests/benchmarks/src/dataloading/Dataloading.cpp
@@ -0,0 +1,22 @@
+#include <nanobench.h>
+
+#include "GameManager.hpp"
+#include <snitch/snitch_macros_test_case.hpp>
+
+using namespace OpenVic;
+
+TEST_CASE("Dataloading benchmark", "[benchmarks][benchmark-dataloading]") {
+	std::filesystem::path root = Dataloader::search_for_game_path();
+	Dataloader::path_vector_t roots { root };
+
+	ankerl::nanobench::Bench().epochs(10).run("Dataloading", [&] {
+		OpenVic::GameManager game_manager { []() {} };
+
+		game_manager.set_roots(roots);
+		game_manager.load_definitions(
+			[](std::string_view key, Dataloader::locale_t locale, std::string_view localisation) -> bool {
+				return true;
+			}
+		);
+	});
+}
