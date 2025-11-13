980fac828d6c877777ae55a851a59d85457e54ae
Spartan322 <Megacake1234@gmail.com>
2025-11-13 04:18:37 -0500

    Add ErrorSet for returning all reported errors

---
+++ src/openvic-simulation/utility/ErrorSet.hpp
@@ -0,0 +1,147 @@
+#pragma once
+
+#include <bitset>
+#include <cstddef>
+
+#include "openvic-simulation/utility/Containers.hpp"
+#include "openvic-simulation/utility/Error.hpp"
+
+namespace OpenVic {
+	// Set of all reported errors, if empty operates as if set to Error::OK
+	struct ErrorSet : private std::bitset<static_cast<std::size_t>(Error::MAX) - 1> {
+		ErrorSet() = default;
+		ErrorSet(ErrorSet const&) = default;
+		ErrorSet(ErrorSet&&) = default;
+		ErrorSet& operator=(ErrorSet const&) = default;
+		ErrorSet& operator=(ErrorSet&&) = default;
+
+		ErrorSet(Error error) {
+			if (error != Error::OK) {
+				set(static_cast<std::size_t>(error) - 1, true);
+			}
+		}
+
+		ErrorSet& operator=(Error error) {
+			reset();
+			if (error != Error::OK) {
+				set(static_cast<std::size_t>(error) - 1, true);
+			}
+			return *this;
+		}
+
+		using bitset::all;
+		using bitset::any;
+		using bitset::count;
+		using bitset::none;
+		using bitset::size;
+
+		bool operator[](Error error) const {
+			if (error == Error::OK) {
+				return none();
+			}
+
+			return bitset::operator[](static_cast<std::size_t>(error) - 1);
+		}
+
+		bool operator[](ErrorSet const& errors) const {
+			if (errors.none()) {
+				return none();
+			}
+
+			return (*this & errors) == errors;
+		}
+
+		bool operator==(ErrorSet const& rhs) const {
+			return bitset::operator==(rhs);
+		}
+
+		bool operator==(Error rhs) const {
+			return operator[](rhs);
+		}
+
+		bool test(Error error) const {
+			return operator[](error);
+		}
+
+		bool test(ErrorSet const& errors) const {
+			return operator[](errors);
+		}
+
+		ErrorSet& operator|=(ErrorSet const& rhs) {
+			bitset::operator|=(rhs);
+			return *this;
+		}
+
+		ErrorSet operator|=(Error rhs) {
+			if (rhs != Error::OK) {
+				set(static_cast<std::size_t>(rhs) - 1, true);
+			}
+			return *this;
+		}
+
+		friend inline ErrorSet operator|(ErrorSet const& lhs, ErrorSet const& rhs) {
+			ErrorSet result { lhs };
+			result |= rhs;
+			return result;
+		}
+
+		friend inline ErrorSet operator|(ErrorSet const& lhs, Error rhs) {
+			ErrorSet result { lhs };
+			result |= rhs;
+			return result;
+		}
+
+		friend inline ErrorSet operator|(Error lhs, ErrorSet const& rhs) {
+			ErrorSet result { rhs };
+			result |= lhs;
+			return result;
+		}
+
+		memory::string to_bitset_string(memory::string::value_type zero = '0', memory::string::value_type one = '1') const {
+			return bitset::to_string<memory::string::value_type, memory::string::traits_type, memory::string::allocator_type>(
+				zero, one
+			);
+		}
+
+		memory::string to_string() const {
+			memory::string result;
+			if (none()) {
+				result.append(as_string(Error::OK));
+				return result;
+			}
+
+			{
+				std::size_t str_size = 0;
+				for (std::size_t i = 1; i <= size(); i++) {
+					if (!(*this).bitset::operator[](i - 1)) {
+						continue;
+					}
+
+					if (str_size > 0) {
+						str_size += 2; // for ", "
+					}
+
+					str_size += as_string(static_cast<Error>(i)).size();
+				}
+
+				result.reserve(str_size);
+			}
+
+			for (std::size_t i = 1; i <= size(); i++) {
+				if (!(*this).bitset::operator[](i - 1)) {
+					continue;
+				}
+
+				if (result.size() > 0) {
+					result.append(", ");
+				}
+
+				result.append(as_string(static_cast<Error>(i)));
+			}
+			return result;
+		}
+
+	private:
+		using bitset::bitset;
+	};
+}
---
+++ tests/src/utility/ErrorSet.cpp
@@ -0,0 +1,114 @@
+#include "openvic-simulation/utility/ErrorSet.hpp"
+
+#include <string_view>
+
+#include "openvic-simulation/utility/Error.hpp"
+
+#include <snitch/snitch_macros_check.hpp>
+#include <snitch/snitch_macros_test_case.hpp>
+
+using namespace OpenVic;
+
+TEST_CASE("ErrorSet Constructor methods", "[ErrorSet][ErrorSet-constructor]") {
+	CHECK(ErrorSet {}.none());
+	CHECK(ErrorSet { Error::OK }.none());
+	CHECK(ErrorSet { Error::FAILED }.any());
+
+	ErrorSet set_1;
+	ErrorSet set_2 = Error::OK;
+	ErrorSet set_3 = Error::BUG;
+
+	CHECK(set_1.none());
+	CHECK(set_2.none());
+	CHECK(set_3.any());
+	CHECK(set_3[Error::BUG]);
+	CHECK_FALSE(set_3[Error::FAILED]);
+
+	ErrorSet set_4 = set_3;
+
+	CHECK(set_4.any());
+	CHECK(set_4[Error::BUG]);
+	CHECK_FALSE(set_4[Error::FAILED]);
+
+	ErrorSet set_5 = std::move(set_4);
+
+	CHECK(set_5.any());
+	CHECK(set_5[Error::BUG]);
+	CHECK_FALSE(set_5[Error::FAILED]);
+}
+
+TEST_CASE("ErrorSet Operators", "[ErrorSet][ErrorSet-operators]") {
+	ErrorSet set_ok = Error::OK;
+	ErrorSet set_bug = Error::BUG;
+	ErrorSet set_multiple = Error::BUG;
+	set_multiple |= Error::FAILED;
+
+	CHECK(set_ok[Error::OK]);
+	CHECK_FALSE(set_ok[Error::BUG]);
+	CHECK_FALSE(set_ok[Error::FAILED]);
+	CHECK_FALSE(set_ok[Error::ALREADY_EXISTS]);
+	CHECK(set_ok == Error::OK);
+	CHECK_FALSE(set_ok == Error::BUG);
+	CHECK_FALSE(set_ok == Error::FAILED);
+	CHECK_FALSE(set_ok == Error::ALREADY_EXISTS);
+	CHECK_FALSE(set_ok == set_bug);
+	CHECK_FALSE(set_ok == set_multiple);
+	CHECK_FALSE(set_ok[set_bug]);
+	CHECK_FALSE(set_ok[set_multiple]);
+
+	CHECK_FALSE(set_bug[Error::OK]);
+	CHECK(set_bug[Error::BUG]);
+	CHECK_FALSE(set_bug[Error::FAILED]);
+	CHECK_FALSE(set_bug[Error::ALREADY_EXISTS]);
+	CHECK_FALSE(set_bug == Error::OK);
+	CHECK(set_bug == Error::BUG);
+	CHECK_FALSE(set_bug == Error::FAILED);
+	CHECK_FALSE(set_bug == Error::ALREADY_EXISTS);
+	CHECK_FALSE(set_bug == set_ok);
+	CHECK_FALSE(set_bug == set_multiple);
+	CHECK_FALSE(set_bug[set_ok]);
+	CHECK_FALSE(set_bug[set_multiple]);
+
+	CHECK_FALSE(set_multiple[Error::OK]);
+	CHECK(set_multiple[Error::BUG]);
+	CHECK(set_multiple[Error::FAILED]);
+	CHECK_FALSE(set_multiple[Error::ALREADY_EXISTS]);
+	CHECK_FALSE(set_multiple == Error::OK);
+	CHECK(set_multiple == Error::BUG);
+	CHECK(set_multiple == Error::FAILED);
+	CHECK_FALSE(set_multiple == Error::ALREADY_EXISTS);
+	CHECK_FALSE(set_multiple == set_bug);
+	CHECK_FALSE(set_multiple == set_ok);
+	CHECK(set_multiple[set_bug]);
+	CHECK_FALSE(set_multiple[set_ok]);
+
+	CHECK((set_multiple | Error::ALREADY_EXISTS)[Error::ALREADY_EXISTS]);
+	CHECK_FALSE((set_multiple | Error::ALREADY_EXISTS)[Error::CONNECTION_ERROR]);
+
+	set_multiple |= Error::CONNECTION_ERROR;
+	CHECK(set_multiple[Error::BUG]);
+	CHECK(set_multiple[Error::FAILED]);
+	CHECK(set_multiple[Error::CONNECTION_ERROR]);
+
+	set_multiple |= Error::OK;
+	CHECK(set_multiple[Error::BUG]);
+	CHECK(set_multiple[Error::FAILED]);
+	CHECK(set_multiple[Error::CONNECTION_ERROR]);
+}
+
+TEST_CASE("ErrorSet to_string", "[ErrorSet][ErrorSet-to_string]") {
+	using namespace std::string_view_literals;
+
+	ErrorSet set_ok = Error::OK;
+	ErrorSet set_bug = Error::BUG;
+	ErrorSet set_multiple = Error::BUG;
+	set_multiple |= Error::FAILED;
+
+	CHECK(set_ok.to_string() == "OK"sv);
+	CHECK(set_bug.to_string() == "Bug"sv);
+	CHECK(set_multiple.to_string() == "Failed, Bug"sv);
+
+	CHECK(set_ok.to_bitset_string() == "00000000000000000000000"sv);
+	CHECK(set_bug.to_bitset_string() == "10000000000000000000000"sv);
+	CHECK(set_multiple.to_bitset_string() == "10000000000000000000001"sv);
+}
